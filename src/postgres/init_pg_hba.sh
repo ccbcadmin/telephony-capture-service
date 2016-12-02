5356cfe774e2ede8e443056e7bd85c1695744472
Roderick Monk <Rod@Rodericks-MacBook-Pro.local>
2016-12-02 09:25:25 -0800

    add 10.211.x.x/16 as trusted IP address

--- src/postgres/init_pg_hba.sh
+++ src/postgres/init_pg_hba.sh
@@ -6,7 +6,9 @@ rm $PGDATA/pg_hba.conf
 { echo; echo "local replication all trust"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
 { echo; echo "host all all 127.0.0.1/32 trust"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
 { echo; echo "host replication all 127.0.0.1/32 trust"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
-{ echo; echo "host replication all 172.0.0.0/8 trust"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
 { echo; echo "host all all 172.0.0.1/8 trust"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
-{ echo; echo "host all all 10.211.0.1/16 md5"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
+{ echo; echo "host replication all 172.0.0.0/8 trust"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
+{ echo; echo "host all all 10.211.0.1/16 trust"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
+{ echo; echo "host replication all 10.211.0.1/16 trust"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
 { echo; echo "host all all 192.168.0.1/16 md5"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
+{ echo; echo "host replication all 192.168.0.1/16 md5"; } | gosu postgres tee -a "$PGDATA/pg_hba.conf" > /dev/null
