a7db6d2448ec6aa0e92c19b91268935291eb5aae
Roderick Monk <Rod@Rodericks-MacBook-Pro.local>
2016-12-05 13:35:50 -0800

    Create image tcs-node and use everywhere

---
+++ src/tms-simulator/tms-simulator.ts
@@ -0,0 +1,44 @@
+"use strict";
+
+const $ = require('../share/constants');
+const server_socket_1 = require('../share/server-socket');
+const util_1 = require('../share/util');
+const routineName = 'tms-simulator';
+const envalid = require('envalid');
+const { str } = envalid;
+const env = envalid.cleanEnv(process.env, {
+    TMS_PORT: str()
+});
+
+const net = require('net');
+process.on('SIGTERM', () => {
+    console.log(`${routineName} terminated`);
+    process.exit(0);
+});
+process.on('SIGINT', () => {
+    console.log(`Ctrl-C received. ${routineName} terminated`);
+    process.exit(0);
+});
+
+let recordCount = 0;
+let leftOver = '';
+const dataDump = (data) => {
+    const unprocessedData = leftOver.slice(0) + data.slice(0);
+    const crLfIndexOf = unprocessedData.indexOf($.CRLF);
+    const msg = unprocessedData.match(/\x00\x02\x00\x00\x00\x00(.+)\x0d\x0a/);
+    if (msg) {
+        if (++recordCount % 20 === 5)
+            process.stdout.write('\b-');
+        else if (recordCount % 20 === 10)
+            process.stdout.write('\b\\');
+        else if (recordCount % 20 === 15)
+            process.stdout.write('\b|');
+        else if (recordCount % 20 === 0)
+            process.stdout.write('\b/');
+        leftOver = unprocessedData.slice(crLfIndexOf + 2);
+    }
+    else {
+        leftOver = unprocessedData.slice(0);
+    }
+};
+new server_socket_1.ServerSocket(routineName, util_1.networkIP, env.TMS_PORT, dataDump);
