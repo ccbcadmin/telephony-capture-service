86781b44962ed302a966e86b3036463d9a044a7c
Roderick Monk <Rod@Rodericks-MacBook-Pro.local>
2017-01-08 21:06:20 -0800

    Jenkins

--- .project.bash
+++ .project.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 # I put a variable in my scripts named PROGNAME which
 # holds the name of the program being run.  You can get this
 # value from the first item on the command line ($0).
