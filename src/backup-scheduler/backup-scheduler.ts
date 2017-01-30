f1163c954c14021d315056cc5626a3b7fd2fee62
Roderick Monk <Rod@Rodericks-MacBook-Pro.local>
2017-01-30 09:35:58 -0800

    Only backup pg1

--- src/backup-scheduler/backup-scheduler.ts
+++ src/backup-scheduler/backup-scheduler.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const routineName = 'barman';
+const routineName = 'backup-scheduler';
 
 // Ensure the presence of required environment variables
 const envalid = require('envalid');
@@ -11,28 +11,19 @@ const env = envalid.cleanEnv(process.env, {
 
 const exec = require('child_process').exec;
 
-// At startup, arbitrarily assume pg1
-let activePostgresContainer = 'pg1';  
-
-// Attempt to backup both pg1 and pg2
 const barmanBackup = () => {
-	console.log(`backup ${activePostgresContainer}`);
-	exec(`barman backup ${activePostgresContainer}`, error => {
+	console.log(`Start Database Backup`);
+	exec(`barman backup pg1`, error => {
 		if (error) {
-			console.log (`Unable to backup ${activePostgresContainer}: ${error}`);
-			// If here, then first backup attempt failed - try the other pg container
-			activePostgresContainer = activePostgresContainer === 'pg1' ? 'pg2' : 'pg1';
-			console.log(`barman backup ${activePostgresContainer}`);
-			exec(`barman backup ${activePostgresContainer}`);
+			console.log(`Unable to backup pg1: JSON.stringify(${error},null,4)`);
 		}
 	});
 }
 
-console.log('Start cron');
 const spawn = require('child_process').spawn;
 exec('cron');
 
-exec('barman cron');
+// exec('barman cron');
 
 process.on('SIGTERM', () => {
 	console.log(`\r${routineName}: Terminated`);
@@ -44,8 +35,9 @@ process.on('SIGINT', () => {
 	process.exit(0);
 });
 
-require('node-schedule').scheduleJob(env.BACKUP_SCHEDULE, barmanBackup);
-
 console.log(`${routineName}: Started`);
 
-setInterval(() => { }, 5000);
+// Let everything stabilize and then trigger the backup schedule
+setInterval(() => {
+	require('node-schedule').scheduleJob(env.BACKUP_SCHEDULE, barmanBackup);
+ }, 5000);
