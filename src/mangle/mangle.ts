c0f4b03d053b18056994d7e67a5ae6f22a8f1188
Roderick Monk <Rod@Rodericks-MacBook-Pro.local>
2017-01-30 17:23:47 -0800

    Fix Mangle Issue

--- src/mangle/mangle.ts
+++ src/mangle/mangle.ts
@@ -12,6 +12,7 @@ const dir = require('node-dir');
 const envalid = require('envalid');
 const { str, num } = envalid;
 const env = envalid.cleanEnv(process.env, {
+	SMDR_DATA: str(),
 	MANGLE_SOURCE_DIRECTORY: str(),
 	MANGLE_TARGET_DIRECTORY: str()
 });
@@ -72,7 +73,7 @@ const replicateSmdrFile = (smdrFileName: string) => {
 	let filePart = smdrFileName.split(pathSeparator());
 	const inputFileNameParts = filePart[filePart.length - 1].split('.');
 	const outputFile = `${inputFileNameParts[0]}.${zeroPad(Number(inputFileNameParts[1]) + 1, 3)}`;
-	const outputPath = [env.MANGLE_TARGET_DIRECTORY, outputFile].join(pathSeparator());
+	const outputPath = [env.SMDR_DATA, env.MANGLE_TARGET_DIRECTORY, outputFile].join(pathSeparator());
 
 	process.stdout.write(`Mangling ${smdrFileName} to ${outputPath}: `);
 
