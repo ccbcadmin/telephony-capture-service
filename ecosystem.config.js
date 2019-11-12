d81f689c91bc99aa00ade19976d60d98bcccc7c6
roderickmonk <roderickmonk@gmail.com>
2019-11-12 19:27:31 +0000

    gitzap

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,18 @@ module.exports = {
       watch: false,
       max_memory_restart: "1G",
       env: {
-        NODE_ENV: "development"
+        NODE_ENV: "development",
+        BACKUP_SCHEDULE: "0 9 * * *",
+        BARMAN_HOME: "/home/barman",
+        DATABASE: "prod",
+        DB_QUEUE: "PROD_DB_QUEUE",
+        PBX_SIMULATOR_SOURCE_DIRECTORY: "smdr-data-005",
+        PBX_SIMULATOR_INPUT_FREQUENCY: 20,
+        TCS_PORT: 3456,
+        TMS_ACTIVE: 1,
+        TMS_HOST: "localhost",
+        TMS_PORT: 6543,
+        TMS_QUEUE: "PROD_TMS_QUEUE"
       },
       env_production: {
         NODE_ENV: "production"
@@ -51,6 +62,21 @@ module.exports = {
     {
       name: "tms-interface",
       script: "./lib/tms-interface/tms-interface.js",
+      args: "",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "tms-simulator",
+      script: "./lib/tms-simulator/tms-simulator.js",
       args: "",
       instances: 1,
       autorestart: true,
